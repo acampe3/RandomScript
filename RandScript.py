6e20bff046740cb297a3a9d39013bbbe8cb45ea9
Alex <campelinyo@gmail.com>
2018-02-28 16:29:30 +0100

    Tercera version del script, funcion comprueba_cero creada

--- RandScript.py
+++ RandScript.py
@@ -7,17 +7,22 @@ import copy
 def delete_first(arguments):
 	deleted = copy.deepcopy(arguments)
 	del deleted[0]
-	return deleted;
+	return deleted
 	
 def choose(argu_del):
-	ale=random.choice(argu_del)
-	return ale;
-
+	ale = random.choice(argu_del)
+	return ale
+	
+def comprueba_cero(argu_del):
+	total_words = len(argu_del)
+	zero        = (total_words==0)
+	return zero
 
 arguments=sys.argv
 argu_del=delete_first(arguments)
+void = comprueba_cero(argu_del)
 
-if (len(arguments)==1):
+if (void):
 	print("Faltan argumentos")
 else:
 	chosen=choose(argu_del)
